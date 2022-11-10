9614fde80b19a7269f6e5b74b6baf553d708fc12
Destiny Boone <destaj.db@gmail.com>
2022-11-09 19:48:24 -0500

    sending a post request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,11 @@ export class AppComponent implements OnInit {
   ngOnInit() {}
 
   onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
-    console.log(postData);
+    // Send Http request - angular will automatically convert our postData to json for us, which is what you normally provide in an http request (json, not a js/ts object!)
+    this.http.post('https://angularsandboxhttp-default-rtdb.firebaseio.com/posts.json', postData)
+      .subscribe(responseData => { // need to subscribe and get the response in order for the http request to even send in the first place; if you do not subscribe, the http request will not get sent! do not need to unsubscribe
+        console.log(responseData);
+      });
   }
 
   onFetchPosts() {
